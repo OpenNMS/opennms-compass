617c4be7553f3f6a1ea78d77ebbceab26b156ad6
Benjamin Reed <ranger@opennms.org>
2015-04-03 15:02:59 -0400

    fix(ios): fix orientation parsing on fresh install

--- hooks/after_prepare/fix_orientation_ios.js
+++ hooks/after_prepare/fix_orientation_ios.js
@@ -1,41 +1,18 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
+var exec = require('child_process').exec;
 var plist = 'platforms/ios/OpenNMS/OpenNMS-Info.plist';
 
-var iphoneModes = [
-	"UIInterfaceOrientationLandscapeLeft",
-	"UIInterfaceOrientationLandscapeRight",
-	"UIInterfaceOrientationPortrait",
-	"UIInterfaceOrientationPortraitUpsideDown",
-];
-
-var ipadModes = [
-	"UIInterfaceOrientationLandscapeLeft",
-	"UIInterfaceOrientationLandscapeRight",
-	"UIInterfaceOrientationPortrait",
-	"UIInterfaceOrientationPortraitUpsideDown",
-];
-
-function getOrientationModeStr(modes) {
-	var s = "<key>$1</key>\n\t<array>\n\t";
-	modes.forEach(function(mode, index) {
-		s += "\t<string>"+mode+"</string>\n\t";
-	});
-	return s;
+function puts(error, stdout, stderr) {
+	if (stdout) {
+		console.log('INFO:  ' + stdout);
+	}
+	if (stderr) {
+		console.log('DEBUG: ' + stderr);
+	}
+	if (error) {
+		console.log('ERROR: ' + error);
+	}
 }
 
-if (fs.existsSync(plist)) {
-	var p = fs.readFileSync(plist, 'utf8');
-	// replace iphone modes
-	p = p.replace(
-		/<key>(UISupportedInterfaceOrientations)<\/key>[\r\n ]*<array>[\s\S]*?(?=<\/array>)/ig,
-		getOrientationModeStr(iphoneModes)
-	);
-	// replace ipad modes
-	p = p.replace(
-		/<key>(UISupportedInterfaceOrientations~ipad)<\/key>[\r\n ]*<array>[\s\S]*?(?=<\/array>)/ig,
-		getOrientationModeStr(ipadModes)
-	);
-	fs.writeFileSync(plist, p, "utf8");
-}
+exec('/usr/libexec/PlistBuddy -c "Merge hooks/after_prepare/fix_orientation_ios.plist" platforms/ios/OpenNMS/OpenNMS-Info.plist', puts);
