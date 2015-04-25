a7d1b771988dd1841cec9d867c75d5062118fe10
Benjamin Reed <ranger@opennms.org>
2015-04-25 13:26:25 -0400

    chore(): fixes for ionic rc3/4

--- hooks/before_build/set_billing_key.js
+++ hooks/before_build/set_billing_key.js
@@ -2,14 +2,9 @@
 
 var fs = require('fs.extra');
 var path = require('path');
+var libxml = require('libxmljs');
 
 var rootdir = process.argv[2];
-
-function replace_string_in_file(filename, to_replace, replace_with) {
-	var data = fs.readFileSync(filename, 'utf8');
-	var result = data.replace(new RegExp(to_replace, 'g'), replace_with);
-	fs.writeFileSync(filename, result, 'utf8');
-}
 
 if (rootdir) {
 	var billingFile = path.join(rootdir, 'platforms/android/res/values/billing_key_param.xml');
@@ -18,8 +13,12 @@ if (rootdir) {
 		var buildjson = path.join(rootdir, 'build-properties.json');
 		var buildobj = JSON.parse(fs.readFileSync(buildjson, 'utf8'));
 		if (buildobj.billingKey) {
-			console.log('setBillingKey: billingKey is configured, replacing');
-			replace_string_in_file(billingFile, '\\$BILLING_KEY', buildobj.billingKey);
+			console.log('setBillingKey: billingKey is configured, replacing file');
+
+			var doc = new libxml.Document();
+			doc.node('resources')
+				.node('string', buildobj.billingKey).attr({'name':'billing_key_param'});
+			fs.writeFileSync(billingFile, doc.toString(), 'utf8');
 		} else {
 			console.log('setBillingKey: no billingKey configured in build-properties.json, skipping');
 		}
