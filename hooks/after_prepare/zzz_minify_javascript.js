740239725e7c1caa857c3def48d605c07eea483c
Benjamin Reed <ranger@opennms.org>
2015-04-30 17:14:13 -0400

    chore(): clean up handling of multi-platform prepare

--- hooks/after_prepare/zzz_minify_javascript.js
+++ hooks/after_prepare/zzz_minify_javascript.js
@@ -4,6 +4,24 @@ var exec = require('child_process').exec;
 
 var rootdir = process.argv[2];
 
+/*
+for (var thing in process.env) {
+	if (process.env.hasOwnProperty(thing) && thing.indexOf('CORDOVA_') === 0) {
+		console.log(thing + ' = ' + process.env[thing]);
+	}
+}
+*/
+
+var platforms = (process.env.CORDOVA_PLATFORMS ? process.env.CORDOVA_PLATFORMS.toLowerCase().split(',') : []);
+//console.log('platforms=',platforms);
 if (rootdir) {
-  exec('gulp prepare');
+	if (platforms.indexOf('android') >= 0 && platforms.indexOf('ios') >= 0) {
+		console.log('* preparing all platforms');
+		exec('gulp prepare');
+	} else {
+		for (var i=0; i < platforms.length; i++) {
+			console.log('* preparing ' + platforms[i]);
+			exec('gulp prepare-' + platforms[i]);
+		}
+	}
 }
