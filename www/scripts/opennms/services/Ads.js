dbaa521803508c5eae9662655f852f748b9c4554
Benjamin Reed <ranger@opennms.org>
2015-05-21 14:35:19 -0400

    fix(ads): fix initialization/update of ads (COMPASS-47)

--- www/scripts/opennms/services/Ads.js
+++ www/scripts/opennms/services/Ads.js
@@ -10,69 +10,83 @@
 		'opennms.services.Config',
 		'opennms.services.Info',
 		'opennms.services.Rest',
+		'opennms.services.Util',
 	])
-	.factory('Ads', ['$q', '$rootScope', '$timeout', '$window', 'Info', 'RestService', 'Settings',
+	.factory('Ads', ['$q', '$rootScope', '$timeout', '$window', '$ionicPlatform', 'Info', 'RestService', 'Settings', 'util',
 		'config.build.admobIdAndroidBanner', 'config.build.admobIdIosBanner', 'config.build.admobIdOtherBanner',
-		function($q, $rootScope, $timeout, $window, Info, RestService, Settings,
+		function($q, $rootScope, $timeout, $window, $ionicPlatform, Info, RestService, Settings, util,
 		androidBannerId, iosBannerId, wpBannerId) {
 
 		console.log('Ads: Initializing.');
 
-		var show = function() {
-			var showAds = Settings.showAds();
-			if (showAds) {
-				if(!Info.isMeridian()) {
-					if ($window.AdMob) {
-						var admobid;
-						if (ionic.Platform.isIOS()) {
-							admobid = iosBannerId;
-						} else if (ionic.Platform.isAndroid()) {
-							admobid = androidBannerId;
-						} else if (ionic.Platform.isWindowsPhone()) {
-							admobid = wpBannerId;
-						}
+		var adsShown = false;
 
-						if (admobid) {
-							console.log('Ads.show: Creating AdMob banner with ID: ' + admobid);
-							$window.AdMob.createBanner({
-								'adId': admobid,
-								'position': AdMob.AD_POSITION.BOTTOM_CENTER,
-								'autoShow': true
-							});
-						} else {
-							console.log('Ads.show: WARNING: Unable to determine platform.');
-						}
-					} else {
-						console.log('Ads.show: AdMob is not available.');
-					}
+		var show = function() {
+			if (adsShown) {
+				// ads are already shown, skip it
+				return;
+			}
+
+			if ($window.AdMob) {
+				var admobid;
+				if (ionic.Platform.isIOS()) {
+					admobid = iosBannerId;
+				} else if (ionic.Platform.isAndroid()) {
+					admobid = androidBannerId;
+				} else if (ionic.Platform.isWindowsPhone()) {
+					admobid = wpBannerId;
+				}
+
+				if (admobid) {
+					console.log('Ads.show: Creating AdMob banner with ID: ' + admobid);
+					$window.AdMob.createBanner({
+						'adId': admobid,
+						'position': AdMob.AD_POSITION.BOTTOM_CENTER,
+						'autoShow': true
+					});
+					adsShown = true;
 				} else {
-					console.log('Ads.show: Remote host is meridian.  Skipping ads.');
+					console.log('Ads.show: WARNING: Unable to determine platform.');
 				}
 			} else {
-				console.log('Ads.show: Settings are configured to not show ads.');
+				console.log('Ads.show: AdMob is not available.');
 			}
 		};
 
 		var hide = function() {
+			if (!adsShown) {
+				// ads are already hidden, skip it
+				return;
+			}
+
 			if ($window.AdMob) {
 				console.log('Ads.hide: Hiding AdMob banner.');
 				$window.AdMob.removeBanner();
 			} else {
 				console.log('Ads.hide: Admob is not available.');
 			}
+			adsShown = false;
 		};
 
-		$rootScope.$on('opennms.settings.updated', function(ev, newSettings, oldSettings, changedSettings) {
-			if (changedSettings.hasOwnProperty('showAds')) {
-				$timeout(function() {
-					if (changedSettings.showAds) {
-						show();
-					} else {
-						hide();
-					}
-				}, 1000);
-			}
-		});
+		var updateAds = function() {
+			$timeout(function() {
+				var showAds = Settings.showAds();
+
+				if (Info.isMeridian()) {
+					console.log('Ads.updateAds: remote host is Meridian.');
+					hide();
+				} else if (showAds) {
+					show();
+				} else {
+					hide();
+				}
+			}, 500);
+		};
+
+		util.onSettingsUpdated(updateAds);
+		util.onProductUpdated(updateAds);
+
+		$ionicPlatform.ready(updateAds);
 
 		return {
 			show: show,
