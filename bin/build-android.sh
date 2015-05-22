71a1ad850480a9691167b30eaf55404957b7d6c6
Benjamin Reed <ranger@opennms.org>
2015-05-22 15:14:49 -0400

    chore(): change hooks to set up Android signing

--- bin/build-android.sh
+++ bin/build-android.sh
@@ -4,5 +4,5 @@ BUILD_NUMBER=`node bin/update-build-number.js`
 
 export ANDROID_VERSION_CODE="$BUILD_NUMBER"
 
-ionic build --release android && bin/sign-android.sh && \
-rsync -avr --progress `find platforms/android -name \*-signed.apk | sort -u | head -n 1` ranger@opennms.org:~/public_html/misc/opennms-android-signed.apk
+ionic build --release android && \
+rsync -avr --progress `find platforms/android -name android-release.apk | sort -u | head -n 1` ranger@opennms.org:~/public_html/misc/opennms-android-signed.apk
