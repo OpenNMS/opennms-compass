f18bc66c89628649cc37b76f96bc5928bdf81b40
Benjamin Reed <ranger@opennms.org>
2015-09-18 12:30:28 -0400

    chore(): trying to clean up ios9 build

---
+++ hooks/after_prepare/allow_arbitrary_loads_ios.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var exec = require('child_process').exec;
+var plist = 'platforms/ios/OpenNMS/OpenNMS-Info.plist';
+
+function puts(error, stdout, stderr) {
+	if (stdout) {
+		console.log('INFO:  ' + stdout);
+	}
+	if (stderr) {
+		console.log('DEBUG: ' + stderr);
+	}
+	if (error) {
+		console.log('ERROR: ' + error);
+	}
+}
+
+if (fs.existsSync('/usr/libexec/PlistBuddy') && fs.existsSync(plist)) {
+	exec('/usr/libexec/PlistBuddy -c "Merge hooks/data/allow_arbitrary_loads.plist" ' + plist, puts);
+}
