db5184e8f95111b24b07b41a018523780f21d46f
Benjamin Reed <ranger@opennms.org>
2015-09-21 15:41:16 -0400

    chore(): remove more typescript leftovers

--- src/scripts/typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-
-/// <reference path="angularjs/angular.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="moment/moment.d.ts" />
-/// <reference path="moment/moment-node.d.ts" />
