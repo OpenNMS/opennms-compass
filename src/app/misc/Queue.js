9ddec3f5048cb45e112b11ae37d309ef1e8e20d2
Benjamin Reed <ranger@opennms.org>
2016-05-03 16:45:06 -0400

    perf(queue): improve latency
    Vastly improve latency of queues with many small requests
    by scheduling another process update on completion of another
    request in the same queue.
    
    Because this takes care of any cascading sets of requests,
    I extended the "idle" queue processing check to 1000ms instead
    of 500ms.

--- src/app/misc/Queue.js
+++ src/app/misc/Queue.js
@@ -2,14 +2,14 @@
 
 var angular = require('angular');
 
-var defaultInterval = 500;
-var defaultMaxRequests = 4;
-var defaultTimeout = 20000;
+var DEFAULT_IDLE_INTERVAL = 1000;
+var DEFAULT_MAX_REQUESTS = 4;
+var DEFAULT_TIMEOUT = 20000;
 
 angular.module('opennms.misc.Queue', [])
-.factory('Queue', function AngularQueue($interval, $log, $q, $rootScope) {
+.factory('Queue', function AngularQueue($interval, $log, $q, $rootScope, $timeout) {
 	var queues = [],
-		interval = defaultInterval,
+		interval = DEFAULT_IDLE_INTERVAL,
 		started = null;
 
 	function printQueueStats(queue, force) {
@@ -29,12 +29,24 @@ angular.module('opennms.misc.Queue', [])
 		}
 	}
 
+	var checkTimeouts = {};
+	function scheduleCheck(queue) {
+		if (checkTimeouts[queue.name]) {
+			$timeout.cancel(checkTimeouts[queue.name]);
+		}
+		checkTimeouts[queue.name] = $timeout(function() {
+			processQueue(queue);
+		});
+	}
+
 	function execute(request) {
 		//$log.info('HTTP.execute: request=' + angular.toJson(request));
 		request.started = Date.now();
+		/*
 		if (__DEVELOPMENT__) {
 			$log.debug('Queue.' + request.queue.name + ': executing, latency=' + (request.started - request.added) + 'ms.');
 		}
+		*/
 		request.queue.inFlight.push(request);
 		return $q.when(request.callback()).then(function(res) {
 			request.deferred.resolve(res);
@@ -44,26 +56,96 @@ angular.module('opennms.misc.Queue', [])
 			return request.deferred.promise;
 		}).finally(function() {
 			request.queue.inFlight.remove(request);
-			printQueueStats(request.queue, true);
+			if (request.queue.inFlight.length === 0) { // eslint-disable-line no-magic-numbers
+				printQueueStats(request.queue, true);
+			}
+			scheduleCheck(request.queue);
 		});
 	}
 
-	function processQueues() {
-		for (var i=0, len=queues.length, queue; i < len; i++) {
-			queue = queues[i];
-			var threshold = Date.now() - queue.timeout;
-			for (var f=queue.inFlight.length - 1, inFlight; f >= 0; f--) { // eslint-disable-line no-magic-numbers
-				inFlight = queue.inFlight[f];
-				if (inFlight && inFlight.started < threshold) {
-					$log.warn('request timed out; this should not happen' + (inFlight.name? ': ' + inFlight.name : ''));
-					inFlight.deferred.reject('timed out');
-					queue.inFlight.remove(inFlight);
+	function getInFlight(request) {
+		return request.queue.inFlight.filter(function(inf) {
+			return inf.name === request.name;
+		});
+	}
+
+	/* eslint-disable no-magic-numbers */
+	function getNextRequest(queue) {
+		var len = queue.pending.length;
+		/*
+		if (__DEVELOPMENT__) {
+			$log.debug('getNextRequest(' + queue.name + '), len=' + len);
+		}
+		*/
+		if (len === 0) {
+			return null;
+		}
+		for (var i=0, request, existing; i < len; i++) {
+			request = queue.pending[i];
+
+			// if this is a named request, check if there is already one by the same name in-flight
+			// if so, skip it and look for another ready request
+			if (request.name) {
+				existing = getInFlight(request);
+				/*
+				if (__DEVELOPMENT__) {
+					$log.debug('getNextRequest(' + queue.name + '): request=' + request.name + ', existing=' + existing.length);
+				}
+				*/
+				if (existing.length > 0) {
+					continue;
 				}
 			}
-			while (queue.pending.length > 0 && queue.inFlight.length < queue.maxRequests) { // eslint-disable-line no-magic-numbers
-				var request = queue.pending.shift();
-				execute(request);
+
+			queue.pending.splice(i, 1);
+			return request;
+		}
+		return null;
+	}
+	/* eslint-enable no-magic-numbers */
+
+	function processQueue(queue) {
+		var threshold = Date.now() - queue.timeout;
+		for (var f=queue.inFlight.length - 1, inFlight; f >= 0; f--) { // eslint-disable-line no-magic-numbers
+			inFlight = queue.inFlight[f];
+			if (inFlight && inFlight.started < threshold) {
+				$log.warn('request timed out; this should not happen' + (inFlight.name? ': ' + inFlight.name : ''));
+				inFlight.deferred.reject('timed out');
+				queue.inFlight.remove(inFlight);
 			}
+		}
+
+		var nextRequest = null;
+		do {
+			if (queue.inFlight.length >= queue.maxRequests) {
+				/*
+				if (__DEVELOPMENT__) {
+					$log.debug('Queue.' + queue.name + ': ' + queue.inFlight.length + ' in-flight requests hit the maximum request threshold (' + queue.maxRequests + ')');
+				}
+				*/
+				break;
+			}
+
+			nextRequest = getNextRequest(queue);
+			/*
+			if (__DEVELOPMENT__) {
+				$log.debug('Queue.' + queue.name + ': next request=' + (nextRequest? nextRequest.name : 'null' ));
+			}
+			*/
+			if (nextRequest === null) {
+				if (queue.pending.length > 0) { // eslint-disable-line no-magic-numbers
+					$log.debug('Queue.' + queue.name + ': skipping ' + queue.pending.length + ' requests because they have previous requests already in-flight.');
+				}
+				break;
+			}
+			execute(nextRequest);
+		} while (nextRequest !== null);
+		printQueueStats(queue);
+	}
+
+	function processQueues() {
+		for (var i=0, len=queues.length; i < len; i++) {
+			processQueue(queues[i]);
 		}
 		printQueuesStats();
 	}
@@ -108,8 +190,8 @@ angular.module('opennms.misc.Queue', [])
 		this.name = options.name || Date.now();
 		this.pending = [];
 		this.inFlight = [];
-		this.maxRequests = this.options.maxRequests || defaultMaxRequests;
-		this.timeout = this.options.timeout || defaultTimeout;
+		this.maxRequests = this.options.maxRequests || DEFAULT_MAX_REQUESTS;
+		this.timeout = this.options.timeout || DEFAULT_TIMEOUT;
 	}
 	Queue.prototype.add = function addToQueue(callback, name) {
 		var self = this,
@@ -173,6 +255,10 @@ angular.module('opennms.misc.Queue', [])
 				$log.warn('setInterval(' + i + ') is not valid!');
 			}
 			interval = int;
+			if (started) {
+				stop();
+				start();
+			}
 		},
 		create: create,
 		get: get,
